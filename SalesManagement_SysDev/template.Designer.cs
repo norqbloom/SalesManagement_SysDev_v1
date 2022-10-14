e95328ce28a740285e495b10fcede5168578c898
shoki <syoukintv0920@gmail.com>
2022-10-14 15:42:52 +0900

    a

--- SalesManagement_SysDev/template.Designer.cs
+++ SalesManagement_SysDev/template.Designer.cs
@@ -33,7 +33,9 @@ namespace SalesManagement_SysDev
             this.panel1 = new System.Windows.Forms.Panel();
             this.btn_client = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.sidepanel.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // sidepanel
@@ -61,18 +63,28 @@ namespace SalesManagement_SysDev
             this.btn_client.Dock = System.Windows.Forms.DockStyle.Top;
             this.btn_client.Location = new System.Drawing.Point(0, 100);
             this.btn_client.Name = "btn_client";
-            this.btn_client.Size = new System.Drawing.Size(250, 54);
+            this.btn_client.Size = new System.Drawing.Size(250, 45);
             this.btn_client.TabIndex = 1;
             this.btn_client.Text = "顧客管理";
             this.btn_client.UseVisualStyleBackColor = true;
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.button1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 154);
+            this.panel2.Location = new System.Drawing.Point(0, 145);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(250, 103);
             this.panel2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // template
             // 
@@ -83,6 +95,7 @@ namespace SalesManagement_SysDev
             this.Name = "template";
             this.Text = "template";
             this.sidepanel.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -93,5 +106,6 @@ namespace SalesManagement_SysDev
         private System.Windows.Forms.Button btn_client;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
     }
 }
