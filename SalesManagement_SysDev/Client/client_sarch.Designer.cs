ea627d5b32a72e20a20c439158269496dd97d328
Syosuke2A03SI <c1045@oic.osaka.jp>
2022-10-28 17:08:46 +0900

    a

--- SalesManagement_SysDev/Client/client_sarch.Designer.cs
+++ SalesManagement_SysDev/Client/client_sarch.Designer.cs
@@ -178,7 +178,7 @@ namespace SalesManagement_SysDev.Client
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 589);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button2);
