0b7a878ca4bdca0571f4c32f371b62dfc86d3b62
kazuki2a08 <c1144@oic.jp>
2022-12-15 15:51:17 +0900

    a

--- SalesManagement_SysDev/SalesManagement_DevContext.cs
+++ SalesManagement_SysDev/SalesManagement_DevContext.cs
@@ -28,7 +28,7 @@ namespace SalesManagement_SysDev
         public DbSet<T_ArrivalDetail> T_ArrivalDetails { get; set; }
         public DbSet<T_Chumon> T_Chumons { get; set; }
         public DbSet<T_ChumonDetail> T_ChumonDetails { get; set; }
-        public DbSet<T_Chhistory> M_Chhistory { get; set; }
+        public DbSet<T_Chhistory> T_Chhistory { get; set; }
         public DbSet<T_Hattyu> T_Hattyus { get; set; }
         public DbSet<T_HattyuDetail> T_HattyuDetails { get; set; }
         public DbSet<T_LoginHistory> T_LoginHistorys { get; set; }
