9f714e677c626d319f0b767edce97f71905a60b4
sota2a17 <c1262@oic.jp>
2022-12-19 10:44:51 +0900

    a

--- SalesManagement_SysDev/Form/DbAccess/OrderDateAccess.cs
+++ SalesManagement_SysDev/Form/DbAccess/OrderDateAccess.cs
@@ -102,23 +102,6 @@ namespace SalesManagement_SysDev
         //          ：削除成功の場合True
         //          ：削除失敗の場合False
         ///////////////////////////////
-       /* public bool DeleteOrderData(string delOrderID)
-        {
-            try
-            {
-                var context = new SalesManagement_DevContext();
-                var Order = context.T_Orders.Single(x => x.OrID.ToString() == delOrderID);
-                context.T_Orders.Remove(Order);
-                context.SaveChanges();
-                context.Dispose();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "例外エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return false;
-            }
-        }*/
         public bool DeleteOrderData(T_Order selectionCondition)
         {
             try
@@ -161,49 +144,10 @@ namespace SalesManagement_SysDev
 
             try
             {
-
                 var context = new SalesManagement_DevContext();
                 orders = context.T_Orders.Where(x => x.OrFlag == radioint).ToList();
                 context.SaveChanges();
                 context.Dispose();
-
-                //var tb = from t1 in context.T_Orders
-                //         join t2 in context.M_SalesOffices
-                //         on t1.SoID equals t2.SoID
-                //         join t3 in context.M_Employees
-                //         on t1.EmID equals t3.EmID
-                //         join t4 in context.M_Clients
-                //         on t1.ClID equals t4.ClID
-                //         //where t1.OrStateFlag == StateFlg
-                //         where t1.OrFlag == radioint
-                //         select new
-                //         {
-                //             t1.OrID,
-                //             t2.SoID,
-                //             t3.EmID,
-                //             t4.ClID,
-                //             t1.ClCharge,
-                //             t1.OrDate,
-                //             t1.OrStateFlag,
-                //             t1.OrFlag,
-                //             t1.OrHidden
-                //         };
-
-                //foreach (var p in tb)
-                //{
-                //    orders.Add(new T_Order()
-                //    {
-                //        OrID = p.OrID,
-                //        SoID = p.SoID,
-                //        EmID = p.EmID,
-                //        ClID = p.ClID,
-                //        ClCharge = p.ClCharge,
-                //        OrDate = p.OrDate,
-                //        OrStateFlag = p.OrStateFlag,
-                //        OrFlag = p.OrFlag,
-                //        OrHidden = p.OrHidden
-                //    });
-                //}
             }
             catch (Exception ex)
             {
@@ -211,7 +155,6 @@ namespace SalesManagement_SysDev
             }
             return orders;
         }
-
     }
 }
     
--- SalesManagement_SysDev/Form/DbAccess/ProductDataAccess.cs
+++ SalesManagement_SysDev/Form/DbAccess/ProductDataAccess.cs
@@ -133,49 +133,13 @@ namespace SalesManagement_SysDev
         ///////////////////////////////
         public List<M_Product> GetProductDataDsp(int radioint)
         {
-            List<M_Product> product = new List<M_Product>();
+            List<M_Product> product = new  List<M_Product>();
 
             try
             {
                 var context = new SalesManagement_DevContext();
-                var tb = from t1 in context.M_Products
-                         join t2 in context.M_Makers
-                         on t1.MaID equals t2.MaID
-                         join t3 in context.M_SmallClassifications
-                         on t1.ScID equals t3.ScID
-                         where t1.PrFlag == radioint
-                         select new
-                         {
-                             t1.PrID,
-                             t2.MaID,
-                             t1.PrName,
-                             t1.Price,
-                             t1.PrSafetyStock,
-                             t3.ScID,
-                             t1.PrModelNumber,
-                             t1.PrColor,
-                             t1.PrReleaseDate,
-                             t1.PrFlag,
-                             t1.PrHidden
-                         };
-
-                foreach (var p in tb)
-                {
-                    product.Add(new M_Product()
-                    {
-                        PrID = p.PrID,
-                        MaID = p.MaID,
-                        PrName = p.PrName,
-                        Price = p.Price,
-                        PrSafetyStock = p.PrSafetyStock,
-                        ScID = p.ScID,
-                        PrModelNumber = p.PrModelNumber,
-                        PrColor = p.PrColor,
-                        PrReleaseDate = p.PrReleaseDate,
-                        PrFlag = p.PrFlag,
-                        PrHidden = p.PrHidden
-                    });
-                }
+                product = context.M_Products.Where(x => x.PrFlag == radioint).ToList();
+                context.SaveChanges();
                 context.Dispose();
             }
             catch (Exception ex)
