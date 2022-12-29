5c026e8089d49827c372450f6b0511ef4af47c68
Syosuke2A03SI <c1045@oic.osaka.jp>
2022-12-29 19:50:58 +0900

    　

--- SalesManagement_SysDev/Entity/T_Chumon.cs
+++ SalesManagement_SysDev/Entity/T_Chumon.cs
@@ -4,6 +4,8 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 
@@ -12,16 +14,25 @@ namespace SalesManagement_SysDev
     class T_Chumon
     {
         [Key]
-        public int ChID { get; set; }               //注文ID	
+        [DisplayName("注文ID")]
+        public int ChID { get; set; }               //注文ID
+        [DisplayName("営業所ID")]
         public int SoID { get; set; }               //営業所ID	
+        [DisplayName("社員ID")]
         public int? EmID { get; set; }               //社員ID	
-        public int ClID { get; set; }               //顧客ID	
+        [DisplayName("顧客ID")]
+        public int ClID { get; set; }               //顧客ID
+        [DisplayName("受注ID")]
         public int OrID { get; set; }               //受注ID
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        [DisplayName("注文年月日")]
         public DateTime? ChDate { get; set; }       //注文年月日
-        public int? ChStateFlag { get; set; }    //注文状態フラグ
-        public int ChFlag { get; set; }	//注文管理フラグ
+        [DisplayName("注文状態フラグ")]
+        public int? ChStateFlag { get; set; }    //注文状態フラグ　
+        [DisplayName("注文管理フラグ")]
+        public int ChFlag { get; set; }	//注文管理フラグ　
+        [DisplayName("非表示理由")]
         public String ChHidden { get; set; }	    //非表示理由	
 
  
